c09583c51e4b91e4b342b696970530cf76e75517
Miran Qarachatani <miranararat722@gmail.com>
2024-02-17 18:05:36 -0500

    update the survey index ui

--- Server/Controllers/survey.js
+++ Server/Controllers/survey.js
@@ -5,18 +5,48 @@ let surveyIndex = async (req, res, next) => {
   console.log("survey controller fired");
   // fetch all media docs
   try {
-    const surveyCollection = await Survey.find(); // Use await to wait for the promise to resolve
-    console.log(surveyCollection); // This should now log the actual documents found
+    const surveyCollection = await Survey.find();
+    console.log(surveyCollection);
 
     res.render("index", {
       title: "Survey Index",
       page: "surveyindex",
-      Survey: surveyCollection, // Make sure this matches the variable name expected in your template
+      Survey: surveyCollection,
     });
   } catch (error) {
     console.error("Error fetching surveys:", error);
     res.status(500).send("Error fetching surveys");
   }
+};
+
+let displayCreateForm = (req, res, next) => {
+  res.render("survey/create", { title: "Add New Survey" });
+};
+
+let createSurvey = async (req, res, next) => {
+  await Survey.create(req.body);
+
+  res.redirect("/survey");
+};
+
+let deleteSurvey = async (req, res, next) => {
+  await Survey.findByIdAndDelete(req.params._id);
+
+  res.redirect("/survey");
+};
+
+let displayEditForm = async (req, res, next) => {
+  let survey = await Survey.findById(req.params._id);
+
+  res.render("survey/edit", {
+    title: "Update Survey",
+    survey: survey,
+  });
+};
+
+let updateSurvey = async (req, res, next) => {
+  await Survey.findByIdAndUpdate(req.params._id, req.body);
+  res.redirect("/survey");
 };
 
 module.exports = {
