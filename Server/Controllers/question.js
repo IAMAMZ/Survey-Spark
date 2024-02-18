5f0bccc8656ff12bec396f762b96f0f4ade00284
Miran Qarachatani <miranararat722@gmail.com>
2024-02-18 14:31:16 -0500

    implement POST question, now you can add question to survey

--- Server/Controllers/question.js
+++ Server/Controllers/question.js
@@ -1,10 +1,58 @@
 // get the survey model
 const Survey = require("../Models/Survey");
 
-const displayQuestionCreateForm = (req, res, next) => {
-  res.render("question/create", { title: "Add Question", page: "create" });
+const displayQuestionCreateForm = async (req, res, next) => {
+  let survey = await Survey.findById(req.params.surveyId);
+  res.render("question/create", {
+    title: "Add Question",
+    page: "create",
+    survey: survey,
+  });
+};
+
+const saveSurveyQuestion = async (req, res, next) => {
+  try {
+    // Find the survey by ID
+    let survey = await Survey.findById(req.params.surveyId);
+
+    // if it doesn't exist return 404
+    if (!survey) {
+      return res.status(404).send("Survey not found");
+    }
+
+    // Extract question details from request body
+    const { text, type, options } = req.body;
+
+    // create a  new question object
+    let newQuestion = {
+      text: text,
+      type: type,
+      options: [],
+    };
+
+    // If the question type is 'multiple-choice', parse the options --> we could handle options some other way, it's csv for now
+    if (type === "multiple-choice" && options) {
+      newQuestion.options = options.split(",").map((option) => ({
+        text: option.trim(), // Create an option object for each option
+        value: option.trim(),
+      }));
+    }
+
+    // Add the new question to the survey's questions array
+    survey.questions.push(newQuestion);
+
+    // Save the updated survey
+    await survey.save();
+
+    // Redirect to the survey details page and pass the survey id
+    res.redirect(`/survey/details/${survey._id}`);
+  } catch (error) {
+    console.error("Error saving new question:", error);
+    res.status(500).send("Error saving new question");
+  }
 };
 
 module.exports = {
   displayQuestionCreateForm,
+  saveSurveyQuestion,
 };
--- Server/Routes/question.js
+++ Server/Routes/question.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 const questionController = require("../Controllers/question");
 
-router.get("/:surveyId/questions/add", (req, res, next) => {
-  questionController.displayQuestionCreateForm(req, res, next);
-});
+router.get(
+  "/:surveyId/questions/add",
+  questionController.displayQuestionCreateForm
+);
+
+router.post("/:surveyId/questions/add", questionController.saveSurveyQuestion);
 
 module.exports = router;
