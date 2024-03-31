7189862eeedb6de7fb711bc55bef9cd5de566fb4
Miran Qarachatani <miranariff1@gmail.com>
2024-03-31 14:02:21 -0400

    Make taking survey private

--- Server/Routes/takeSurvey.js
+++ Server/Routes/takeSurvey.js
@@ -5,17 +5,17 @@ const router = express.Router();
 let isAuthenticated = require('../Config/authCheck');
 
 const takeSurveyController = require("../Controllers/takeSurvey");
-router.get("/complete", takeSurveyController.renderSurveyComplete);
+router.get("/complete",isAuthenticated, takeSurveyController.renderSurveyComplete);
 
-router.get("/:surveyId", takeSurveyController.takeSurvey);
+router.get("/:surveyId",isAuthenticated, takeSurveyController.takeSurvey);
 
 router.get(
-  "/:surveyId/sections/:sectionId",
+  "/:surveyId/sections/:sectionId",isAuthenticated,
   takeSurveyController.displaySection
 );
 
 router.post(
-  "/:surveyId/sections/:sectionId/saveResponse",
+  "/:surveyId/sections/:sectionId/saveResponse",isAuthenticated,
   takeSurveyController.handleResponse
 );
 
